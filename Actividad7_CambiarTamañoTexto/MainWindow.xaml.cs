ee7e3301569dff47b74341a44d4184a0ea1f1d7e
Sandra Hurtado <sandra.hurtado16@iesdoctorbalmis.com>
2020-11-18 12:34:18 +0100

    Agregar archivos de proyecto.

---
+++ Actividad7_CambiarTamañoTexto/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Actividad7_CambiarTamañoTexto
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            medianoRadioButton.IsChecked = true;
+        }
+
+        private void medianoRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            textTextBlock.FontSize = 48;
+        }
+
+        private void pequeñoRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            textTextBlock.FontSize = int.Parse((sender as RadioButton).Tag.ToString());
+        }
+
+        private void grandeRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            textTextBlock.FontSize = 72;
+        }
+    }
+}
